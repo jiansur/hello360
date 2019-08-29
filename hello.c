614d2d0b6cb0991199a5b56df917c0fa078d3a81
Jian Huang <j@paralleldomains.com>
2019-08-29 14:23:43 -0400

    adding functionality from fields

--- hello.c
+++ hello.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "fields.h"
 
 int
-main()
+main(int argc, char ** argv)
 {
+  IS is;
+  int i;
 
   printf("hello world\n");
+
+  is = new_inputstruct(argv[1]);
+  if (is == NULL) {
+    perror("input");
+    exit(1);
+  }
+
+  while(get_line(is) >= 0) {
+    for (i = 0; i < is->NF; i++) {
+      printf("%d: %s\n", is->line, is->fields[i]);
+    }
+  }
+
+  jettison_inputstruct(is);
+
   return 0;
 }
