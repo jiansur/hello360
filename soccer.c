419cd907c4cf3e5d103af5df0c0179604c7e6eda
Jian Huang <j@paralleldomains.com>
2019-09-03 14:04:20 -0400

    starting API design. Code doesn't compile

---
+++ soccer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "soccer.h"
+
+stats 
+create_record(int year, int day, int type, char * label)
+{
+
+  stats s;
+
+  s = malloc();
+
+  return NULL;
+}
--- soccer.h
+++ soccer.h
@@ -1,8 +1,21 @@
 #ifndef ___SOCCER_H_
 #define ___SOCCER_H_
 
+typedef struct sta {
 
-void read_record();
+  int year;
+  int day;
+  int type;
+  char * label;
+
+} * stats;
+
+stats create_record(int year, int day, int type, char * label);
+
+void copy_record(stats src, stats dst);
+
+void free_record(stats s);
+
 
 
 #endif
